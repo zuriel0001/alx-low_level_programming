85adff93bab63602d2e0abad85b839ed14659278
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-03-16 16:59:51 +0000

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - prints the size of various types on the computer it is compiled and run on
+ * main - prints the size of various types on the computer size.
  *
  * Return: Always 0 (Success)
  */
