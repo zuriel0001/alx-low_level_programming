39f1e4afe5e63ce439213b99dfd257c2f5806533
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-16 19:17:57 +0000

    fixing errors

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,10 @@
  */
 int main(void)
 {
-	char  art[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char art[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int len = sizeof(art) / sizeof(char);
-	write(write(STDOUT_FILENO, art, len);
+
+	write(STDOUT_FILENO, art, len);
 	return (1);
 }
 
