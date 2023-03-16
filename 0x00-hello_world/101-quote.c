51c3445917e9b3600e33ca18f491317777ddc8ea
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-03-16 19:30:54 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 	char art[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int len = sizeof(art) / sizeof(char);
 
-	write(STDOUT_FILENO, art, len);
+	write(STDERR_FILENO, art, len);
 	return (1);
 }
 
