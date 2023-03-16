dd5f39b0e281ea11d410e0d2ec3b79d2818c6c72
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-16 19:00:47 +0000

    printing line with write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<unistd.h>
+/**
+ * main - printing without using printf and puts
+ *
+ * Return: Alway 1 (Success)
+ */
+int main(void)
+{
+	char  art[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int len = sizeof(art) / sizeof(char);
+	write(write(STDOUT_FILENO, art, len);
+	return (1);
+}
+
