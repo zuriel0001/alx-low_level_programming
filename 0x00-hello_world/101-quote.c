d5c3077eb3b7b4e5e083c1cefd5e7d976818677e
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-03-16 19:20:53 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char art[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char art[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int len = sizeof(art) / sizeof(char);
 
 	write(STDOUT_FILENO, art, len);
