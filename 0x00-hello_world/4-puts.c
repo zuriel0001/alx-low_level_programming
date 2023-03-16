85407eaf1053f58c5979a69610024af7b5bd8a9d
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-16 14:15:05 +0000

    attended to error messages

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#Include <stdio.h>
-int main(void) {puts("\"Programming is like building a multilingual puzzle\n"); return(0);}
+#include < stdio.h >
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
+}
