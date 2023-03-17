c7ae03c19acbeb6e2fe51408e9327236a1f93d77
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-17 15:09:13 +0000

    printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Printing alphabets
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	char alpbt;
+
+	alpbt = 'a';
+	while (aplbt = a, a <= z, a++)
+	{
+		putchar("%c\n", alpbt);
+	}
+	return (0);
+
+}
