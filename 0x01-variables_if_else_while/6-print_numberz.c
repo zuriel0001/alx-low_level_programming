a9da844f919649cecd1afb430d71129da4b080da
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-17 17:36:25 +0000

    listing num with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - listing 0 to 9 using putchar and not setting char
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numz[];
+
+	numz[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int i
+		for (i = 0; 1 < 10; i++)
+		{
+			putchar(numz[i] +'0');
+		}
+	putchar('\n');
+	return (0);
+}
+
