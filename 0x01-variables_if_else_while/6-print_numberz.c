af4de9161f6f4cc454357ca7f97e566bd27cb370
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-03-17 17:40:22 +0000

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	int numz[];
+	int numz[10];
 
-	numz[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	int i
+	numz[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int i;
 		for (i = 0; i < 10; i++)
 		{
 			putchar(numz[i] +'0');
