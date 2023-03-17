852463ff9f6d921acfa6ab983d335b80d24b2756
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-03-17 14:55:42 +0000

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Determine last digit  of a random num.  and compare it to other number
+ * main - Determine last dgit of a random num.  and compare it to other number
  *
  * Return: Always (0) Succes
  */
@@ -14,11 +14,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last_dgt = n % 10;
 
-	if (last_digit > 5)
+	if (last_dgt > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_dgt);
 	}
-	else if (last_digit == 0)
+	else if (last_dgt == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_dgt);
 	}
