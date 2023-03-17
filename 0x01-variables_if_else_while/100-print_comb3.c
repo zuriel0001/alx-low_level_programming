3d0013d3b5abd914a05edfa20d5e77e1f0b4b993
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-17 20:28:58 +0000

    number combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Combination of numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i < 8)
+			{
+				putchar(',');
+				putchat(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
