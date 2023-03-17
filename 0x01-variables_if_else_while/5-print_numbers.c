feaf22bc794a3613eef895ea4966a9d5ac4ac179
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-17 16:57:46 +0000

    edited script on base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Listing base 10 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(0)
+{
+	int a;
+
+	a = 0;
+	for (a = 0, a < 10, a++)
+	{
+		printf("%d\n", a);
+	}
+	return (0);
+}
