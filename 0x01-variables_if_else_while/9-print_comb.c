0ce5c7d2f4567a8985982b3bd627f609e22d3eaf
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-17 19:49:23 +0000

    listing with comma and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Listing numbers with comma and spaces
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
