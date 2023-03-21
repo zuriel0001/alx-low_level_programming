512e0bbac5dd3b12eb1e226c59f89853d6092b77
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-21 22:14:37 +0000

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,37 +10,18 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (; n >= 98; n--)
+		for (; n > 98; n--)
 		{
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
-			if (n <= 9)
-			{
-				_putchar(n + '0');
-			}
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%d ", n);
 		}
+
 	}
-	else
+	else if (n < 98)
 	{
-		for (; n <= 98; n++)
+		for (; n < 98; n++)
 		{
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
-			if (n <= 9)
-			{
-				_putchar(n + '0');
-			}
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%d ", n);
 		}
 	}
-	_putchar('\n');
+	printf("%d\n", n);
 }
