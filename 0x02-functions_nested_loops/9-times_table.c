e3ecb0fc0924c2a926d3cdbb3f2ab2ee7d5ccdf3
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-21 18:27:21 +0000

    nine times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "maonmh"
+/**
+ * times_table - Print 9 times table
+ *
+ * Return:  Always 0 Success
+ */
+void times_table(void)
+{
+	int x, y, multiple;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			result = x * y;
+			if (result > 9)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			else
+			{
+				_putchar(result + '0');
+			}
+			if (y < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
