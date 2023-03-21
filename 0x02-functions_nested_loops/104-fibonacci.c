fed2dac8c3a650b009d433341618c32738acbda8
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-21 23:32:12 +0000

    generate the first 98 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - loop to generate the first 98 Fibonacci numbers
+ *
+ * Return: Alway 0 Success
+ */
+int main(void)
+{
+	int prev = 1;
+	int curr = 2;
+
+	for (int i = 3; i <= 98; i++)
+	{
+		int next_term = prev + curr;
+
+		prev = curr;
+		curr = next_term;
+		printf(", %d", curr);
+	}
+	printf("\n");
+	return (0);
+}
