247ff2be047fa0c254c01dfc4ceb7039df5bd2c1
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-21 16:44:35 +0000

    print the minutes of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: N9thing
+ */
+void jack_bauer(void)
+{
+	int(i, j);
+	int(k, l);
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 3; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					if (i > 2 && j > 3)
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar(l + '0');
+						break;
+					}
+				}
+			}
+		}
+	}
+	_putchar('\n');
+}
