b907121ef6770809769693cf6bec01fbfeccff35
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-21 20:34:41 +0000

    sum 9f integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - add two integers and return the result
+ *
+ * @i: first integer
+ * @j: 2nd integer
+ *
+ * Return: Sum
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+	_putchar('\n');
+}
