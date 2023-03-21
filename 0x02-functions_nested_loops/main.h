a0c56ea434fb0563ab7cb65c91477692641fe261
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-21 13:28:15 +0000

    added _putchar.c file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,4 @@
-#ifndef main_h
-#define main_h
-
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -9,5 +6,3 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-
-#endif
