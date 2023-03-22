725b3974b3cd49156c1a82ab92b902c7e3a3d14c
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-21 19:14:35 -0700

    8-24_hours.c error dbg

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,25 +6,18 @@
  */
 void jack_bauer(void)
 {
-	int i, j;
-	int k, l;
+	int h, m;
 
-	for (i = 0; i <= 2; i++)
+	for (h = 0; h < 24; h++)
 	{
-		for (j = 0; j <= 3; j++)
+		for (m = 0; m < 60; m++)
 		{
-			for (k = 0; k <= 5; k++)
-			{
-				for (l = 0; l <= 9; l++)
-				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(l + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
