1936632de3758faf7bf4c68cd3694e659cbb0faa
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-21 18:35:19 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,7 +18,14 @@ void print_times_table(int n)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			printf("%4d, ", i * j);
+			if (j == 0)
+			{
+				printf("%d", i * j);
+			}
+			else
+			{
+				printf(", %4d", i * j);
+			}
 		}
 		printf("\n");
 	}
