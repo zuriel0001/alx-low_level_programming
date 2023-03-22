bd97fedec91dd5fc72312c7d09899d42eef21f09
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-03-22 01:06:37 +0000

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 void print_times_table(int n)
 {
-  int i;
+  int i, j;
   
   if (n < 0 || n > 15)
   {
@@ -10,7 +10,7 @@ void print_times_table(int n)
   }
   for (i = 0; i <= n; i++)
   {
-    for (int j = 0; j <= n; j++)
+    for (j = 0; j <= n; j++)
     {
       printf("%4d", i * j);
     }
