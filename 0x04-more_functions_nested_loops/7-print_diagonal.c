895cfde0b4a8a23ce6a5f21f74f083251d7c7f0c
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-23 16:53:58 +0000

    lines and diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,10 +2,28 @@
 /**
  * print_diagonal - draws a diagonal line on the terminal
  *
- * @n: integer value 
+ * @n: integer value
  *
  * Reuturn: Always 0.
  */
 void print_diagonal(int n)
 {
-	for (
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 1; i < n; i++)
+	{
+		for (j = 0; j < i; j++)/*The first line has 0 spaces, */
+			/*second line has 1 space, and so on, up to n-1 spaces*/
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
