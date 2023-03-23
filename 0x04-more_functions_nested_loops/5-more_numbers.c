dd447208154c6e1abb1ea9e99f5ef2b0097c002f
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-23 08:00:38 -0700

    10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j > 9)
+			if (j >= 10)
 			{
-				_putchar((j / 10) + '0');
+				_putchar('1');
 			}
 			_putchar((j % 10) + '0');
 		}
