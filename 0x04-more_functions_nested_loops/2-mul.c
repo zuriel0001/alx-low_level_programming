f38485639bd5826c4650dffdb53d2420fddcd944
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-23 04:16:03 -0700

    is a digit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add two integers and return the result
+ *
+ * @i: first integer
+ * @j: 2nd integer
+ *
+ * Return: Sum
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+	return (sum);
+}
