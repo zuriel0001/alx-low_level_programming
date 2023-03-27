f80b9722aa7942aa06c8a779bc7960b3eac07430
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-27 09:38:03 -0700

    print final half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* puts_half - print half  of a string
+*
+* @str: string
+* Description: for odd length, len is (length - 1) / 2
+*/
+void puts_half(char *str)
+{
+	int i, len_half;/* i is string index*/
+	int len = 0;/*length of string str*/
+
+	/* calculate the length of the string */
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		len_half = len / 2;
+	}
+	else
+	{
+		len_half =  (len - 1) / 2;
+	}
+
+	for (i = len_half; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
