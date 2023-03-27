90dad5114f4e8827f4d6f0829972aea450666fe2
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-27 16:46:39 -0700

    a function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,42 +2,43 @@
 #include <limits.h>
 
 /**
- * _atoi - convert a string to an integer
- * @s: string to be converted
- *
- * Return: integer value of string, or 0 if no number is found
- */
+* _atoi - convert a string to an integer
+* @s: string to be converted
+*
+* Return: integer value of string, or 0 if no number is found
+*/
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int digit;
+	int result = 0;
+	int sign = 1;
+	int digit;
 
-    while (*s != '\0')
-    {
-        if (*s == '-')
-            sign *= -1;
+	while (*s != '\0')
+	{
+	if (*s == '-')
+		sign *= -1;
 
-        if (*s >= '0' && *s <= '9')
-        {
-            digit = *s - '0';
+	if (*s >= '0' && *s <= '9')
+	{
+		digit = *s - '0';
 
-            /* Check for overflow */
-            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10))
-            {
-                return (sign == 1 ? INT_MAX : INT_MIN);
-            }
+		/* Check for overflow */
+		if (result > INT_MAX / 10 ||
+		(result == INT_MAX / 10 && digit > INT_MAX % 10))
+		{
+			return (sign == 1 ? INT_MAX : INT_MIN);
+		}
 
-            result = result * 10 + digit;
-        }
+		result = result * 10 + digit;
+	}
 
-        /* Exit loop when a number has been found and processed */
-        if (result != 0 && (*s < '0' || *s > '9'))
-            break;
+	/* Exit loop when a number has been found and processed */
+	if (result != 0 && (*s < '0' || *s > '9'))
+		break;
 
-        s++;
-    }
+	s++;
+	}
 
-    return (result * sign);
+	return (result * sign);
 }
 
