69d4ead5f1a077ba005f222372176145a4af62ea
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-27 14:34:29 -0700

    atoic overflow error resolved

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,39 +1,41 @@
 #include "main.h"
+#include <limits.h>
 /**
 * _atoi -  convert a string to an integer.
 * take into account all - and + signs before the number
 * @s: the string
 *
-* Return: integer value of string, or 0 if no number found
+* Return: integer val. of string, or 0 if no number found
 */
 int _atoi(char *s)
 {
 	int result = 0;
 	int sign = 1;
-	int started = 0;
 
 	while (*s != '\0')
 	{
 		if (*s == '-')
-	{
-		sign *= -1;
-	}
-	else if (*s == '+')
-	{
-		/* ignore */
-	}
-	else if (*s >= '0' && *s <= '9')
-	{
-		result = result * 10 + (*s - '0');
-		started = 1;
-	}
-	else if (started)
-	{
-		/* end of number */
-	break;
-	}
-	s++;
+			sign *= -1;
+
+		if (*s >= '0' && *s <= '9')
+		{
+			int digit = *s - '0';
+
+			/* Check for overflow*/
+			if (result > INT_MAX / 10 ||
+			(result == INT_MAX / 10 && digit > INT_MAX % 10))
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
+
+			result = result * 10 + digit;
+		}
+
+		s++;
 	}
 
-	return (result * sign);
+	return (sign * result);
 }
