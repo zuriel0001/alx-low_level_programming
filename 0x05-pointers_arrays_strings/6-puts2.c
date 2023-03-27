5126eef22b22fee61f459ce7f098b2f64f57d4f3
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-27 10:46:13 -0700

    puts2 char long error debugged

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,27 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a string,
- * starting with the first character
- *
- * @str: string
- */
+* puts2 - prints every other character of a string,
+* starting with the first character
+*
+* @str: string
+*/
 void puts2(char *str)
 {
 	int i;
+	int len = 0;/*length of string str*/
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	/* the length of the string */
+	while (str[len] != '\0')
 	{
-		_putchar(str[i]);
+	len++;
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
