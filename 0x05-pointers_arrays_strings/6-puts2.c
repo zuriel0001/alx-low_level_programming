b0ec8526faae9eef74b5689bb1ffbffce12efa7f
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-27 08:21:44 -0700

    print characters of string increment by 2 starting from 0

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character
+ *
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
