22a582e822f4f56bb4d474d6828ecf7dfe8d016f
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-28 10:31:35 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 Success
+ * */
 int main(void)
 {
-	int myrand;
-	int count;
-	int total;
+	int count, random_char, total;
 
-	srand(time(NULL));
+	srand(time(NULL));/*Seed the random number generator*/
+
 	for (count = 0, total = 2772; total > 122; count++)
 	{
-		myrand = (rand() % 125) + 1;
-		printf("%c", myrand);
-		total -= myrand;
+		/*Generate a random ASCII character between 1 and 125*/
+		random_char = (rand() % 125) + 1;
+		printf("%c", random_char);
+		total -= random_char;
 	}
-	printf("%c", total);
+	printf("%c", total);/*Output the generated password*/
 
 	return (0);
 }
