fe29f0b4759229efa16a986b72acd83a7c80630d
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-03-28 17:20:33 +0000

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 Success
- * */
-int main() 
+
+int main(void)
 {
-    int total = 2772;
-    int count = 0;
-    char password[1024];
-    int random_char;
+	int myrand;
+	int count;
+	int total;
 
-    srand(time(NULL)); /*Seed the random number generator*/
+	srand(time(NULL));
+	for (count = 0, total = 2772; total > 122; count++)
+	{
+		myrand = (rand() % 125) + 1;
+		printf("%c", myrand);
+		total -= myrand;
+	}
+	printf("%c", total);
 
-    while (total > 122) {
-        random_char = (rand() % 125) + 1; /*Generate a random ASCII character between 1 and 125*/
-        password[count] = random_char;
-        total -= random_char;
-        count++;
-    }
-
-    password[count] = total; /*Add the remaining total as the last character of the password*/
-
-    printf("%s\n", password); /* Output the generated password*/
-
-    return 0;
+	return (0);
 }
-
