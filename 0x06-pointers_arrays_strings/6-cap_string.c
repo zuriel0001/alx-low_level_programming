2f921c89438ffe365d7c2549efdfd0387ee828f3
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-29 13:18:44 -0700

    Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* cap_string - capitalizes all words of a string
-* @s: input string
+* cap_string - capitalizes all words of a string with conditions
+* @str: input string
 *
 * Return: pointer to resulting string
 */
@@ -10,18 +10,28 @@ char *cap_string(char *str)
 	int i = 0;
 
 	for (i = 0; str[i]; i++)
-    {
-        if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
-            str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' ||
-            str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
-            str[i - 1] == '{' || str[i - 1] == '}')
-        {
-            if (str[i] >= 'a' && str[i] <= 'z')
-            {
-                str[i] -= 32;
-            }
-        }
-    }
+	{
+		if (i == 0 ||
+			str[i - 1] == ' ' ||
+			str[i - 1] == '\t'||
+			str[i - 1] == '\n'||
+			str[i - 1] == ',' ||
+			str[i - 1] == ';' ||
+			str[i - 1] == '.' ||
+			str[i - 1] == '!' ||
+			str[i - 1] == '?' ||
+			str[i - 1] == '"' ||
+			str[i - 1] == '(' ||
+			str[i - 1] == ')' ||
+			str[i - 1] == '{' ||
+			str[i - 1] == '}')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] -= 32;
+			}
+		}
+	}
 
-    return (str);
+	return (str);
 }
