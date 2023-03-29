8f0fca5159a79184e0bc92af5e04879632d05a02
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-29 13:22:51 -0700

    Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,8 +13,8 @@ char *cap_string(char *str)
 	{
 		if (i == 0 ||
 			str[i - 1] == ' ' ||
-			str[i - 1] == '\t'||
-			str[i - 1] == '\n'||
+			str[i - 1] == '\t' ||
+			str[i - 1] == '\n' ||
 			str[i - 1] == ',' ||
 			str[i - 1] == ';' ||
 			str[i - 1] == '.' ||
