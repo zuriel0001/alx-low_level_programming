db53120c28253ac5f6d36a637936b677725efb65
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-03-31 18:29:10 -0700

    Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,48 +1,49 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_buffer - a function that prints a buffer.
- * @b: b is baffer pointer
- * @size: buffer size in bytes
- * Return: void
- */
+* print_buffer - a function that prints a buffer.
+* @b: b is baffer pointer
+* @size: buffer size in bytes
+* Return: void
+*/
 void print_buffer(char *b, int size)
 {
-	int m, j, i;
+	int j, i;
+	int counter;
 
-    	if (size <= 0)
-    {
-        printf("\n");
-        return;
-    }
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
 
-    for (m = 0; m < size; m += 10)
-    {
-        j = size - m < 10 ? size - m : 10;
-        printf("%08x: ", m);
-        for (i = 0; i < 10; i++)
-        {
-            if (i < j)
-                printf("%02x", *(b + m + i));
-            else
-                printf("  ");
-            if (i % 2)
-            {
-                printf(" ");
-            }
-        }
-        for (i = 0; i < j; i++)
-        {
-            int c = *(b + m + i);
+	for (counter = 0; counter < size; counter += 10)
+	{
+		j = size - counter < 10 ? size - counter : 10;
+		printf("%08x: ", counter);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + counter + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + counter + i);
 
-            if (c < 32 || c > 132)
-            {
-                c = '.';
-            }
-            printf("%c", c);
-        }
-        printf("\n");
-    }
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+	}
 }
 
 
