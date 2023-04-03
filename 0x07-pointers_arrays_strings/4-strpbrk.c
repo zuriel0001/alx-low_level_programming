9bffb4bcb6cfa43eff191004d490a64e920a0ab3
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-04-03 18:35:51 +0000

    Create 4-strpbrk.c
    A function that searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+/**
+ * _strpbrk - a function that searches a string for any of a set of bytes
+ * @s: string input
+ * @accept: string input
+ * Return: pointer to the first occurrence of any character from `accept`
+ *         in `s`, or NULL if no match is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+	return (NULL);
+}
