06c460795e21c11cbe8034c2376c67ffcbabdd2e
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-03 16:43:31 -0700

    a function that locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,34 +1,34 @@
 #include "main.h"
 /**
- * _strstr - finds the first occurrence of the substring needle in the
- *           string haystack
- * @haystack: the string to search in
- * @needle: the substring to search for
- *
- * Return: pointer to the beginning of the located substring, or NULL if the
- *         substring is not found.
- */
+* _strstr - finds the first occurrence of the substring needle in the
+*           string haystack
+* @haystack: the string to search in
+* @needle: the substring to search for
+*
+* Return: pointer to the beginning of the located substring, or NULL if the
+*         substring is not found.
+*/
 char *_strstr(char *haystack, char *needle)
 {
-    int i, j;
+	int i, j;
 
-    if (*needle == '\0')
-        return (haystack);
+	if (*needle == '\0')
+	return (haystack);
 
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        if (haystack[i] == needle[0])
-        {
-            for (j = 1; needle[j] != '\0'; j++)
-            {
-                if (haystack[i + j] != needle[j])
-                    break;
-            }
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 1; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
 
-            if (needle[j] == '\0')
-                return (&haystack[i]);
-        }
-    }
+				if (needle[j] == '\0')
+					return (&haystack[i]);
+		}
+	}
 
-    return (0);
+	return (0);
 }
