e57b1093bc68590196be6b97caa3e861164a9a79
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-04-03 22:58:35 +0000

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,17 +10,16 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
+	int i, j;
 
-	while (*s)
-	{
-		for (i = 0; accept[i]; i++)
-		{
-			if (*s == accept[i])
-				return (s);
-		}
-		s++;
-	}
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = 0; accept[j] != '\0'; j++)
+        {
+            if (s[i] == accept[j])
+                return (s + i);
+        }
+    }
 
-	return ('\0');
+    return ('\0');
 }
