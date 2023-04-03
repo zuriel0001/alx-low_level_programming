39be430190a35a9479bfeec1b43eb3b34435261a
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-04-03 13:35:00 +0000

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 	
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
-		dest[i] = scr[i];
+		dest[i] = src[i];
 		n--;
 	}
+        i++;
 	return (dest);
 }
