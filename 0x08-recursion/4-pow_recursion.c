11bccd0661ce70706512c073f4595b0ac5cff00b
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-05 09:19:24 -0700

    added comments

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,13 +8,22 @@
  */
 int _pow_recursion(int x, int y)
 {
+	/*y is negative, can't compute power*/
 	if (y < 0)
 	{
 		return (-1);
 	}
+
+	/*base case: y is 0  */
+	/* any number exponent 0 is 1*/
 	if (y == 0)
 	{
 		return (1);
 	}
+
+	/**
+	 * multiply x by itself raised to the power of y-1 times
+	 * x is the first integer and y-1 is the last value of x
+	 */
 	return (x * _pow_recursion(x, (y - 1)));
 }
