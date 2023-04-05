ecac974c6ad178bbb93674c731a6d11b29990afc
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-05 16:17:14 -0700

    determine prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,6 +12,8 @@ int is_prime_number(int n)
 	{
 		return (0);
 	}
+	/* check if n is greater than 1 and not divisible by 2, 3, 5, and 7 */
+	/* that makes it a prime number */
 	if (n > 1 &&
 		(n % 2 != 0) &&
 		(n % 3 != 0) &&
