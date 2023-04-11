319db4c4aeda6fb312c002d275fabb9b06ba2aed
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-11 14:14:42 -0700

    fixing error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,14 +3,14 @@
 #include "main.h"
 
 /**
- * str_concat - a function that concatenates two strings.
- *
- * @s1: first string
- * @s2: second string
- *
- * Return: the new string (concatenated)
- *
- */
+* str_concat - a function that concatenates two strings.
+*
+* @s1: first string
+* @s2: second string
+*
+* Return: the new string (concatenated)
+*
+*/
 char *str_concat(char *s1, char *s2)
 {
 	int i = 0, j = 0;
@@ -20,7 +20,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL || s2 == NULL)
 	{
-		return ("");
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	/* defining string s1 & s2 lengths. you could also use just i and j */
 
@@ -29,11 +33,11 @@ char *str_concat(char *s1, char *s2)
 		i++;
 		s1_len++;
 	}
-	 while (s2[j] != '\0')
-	 {
-		 j++;
-	 	s2_len++;
-	 }
+	while (s2[j] != '\0')
+	{
+		j++;
+		s2_len++;
+	}
 	conct_str = malloc((s1_len + (s2_len + 1)) * sizeof(char));
 
 	if (conct_str == NULL)
@@ -44,7 +48,7 @@ char *str_concat(char *s1, char *s2)
 
 	for (j = 0; s2[j] != '\0'; j++)
 
-		conct_str[i + j] = s2[j];
+	conct_str[i + j] = s2[j];
 
 	return (conct_str);
 }
