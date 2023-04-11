476cb096ff87259694904fe09afdbe049568f205
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-04-11 15:46:56 +0000

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,7 +32,7 @@ int **alloc_grid(int width, int height)
 		two_dim_arr[i] = malloc(sizeof(int) * height);
 		if (two_dim_arr == NULL)
 		{
-			for (j = 0; j < width; j++)
+			for (j = 0; j < i; j++)
 			{
 				free(two_dim_arr[j]);
 			}
