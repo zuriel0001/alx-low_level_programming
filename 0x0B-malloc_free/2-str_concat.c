3ebd4b51f50048ee37fb4179fd3ec121d9704e68
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-11 12:48:10 -0700

    fixing output error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -37,9 +37,9 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0'; i++)
 		conct_str[i] = s1[i];
 
-	for (j = 0; s2[j] != '\0'; j++)
+		for (j = 0; s2[j] != '\0'; j++)
 
-		conct_str[i + j] = s2[j];
+			conct_str[i + j] = s2[j];
 
 	conct_str[i + j] = ('\0');
 
