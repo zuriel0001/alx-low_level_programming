18f2617513c0e7f1fcd4d4e8fcf0ea7fad95d12c
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-17 09:54:09 -0700

    fixing error

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,7 +14,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	{
 		return;
 	}
-
+	d = malloc(sizeof(struct dog));
 	/* d is the pointer to the struct dog */
 	d->name = name;
 	d->age = age;
