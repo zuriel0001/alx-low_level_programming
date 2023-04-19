f87f38804a2596ff2d293f67026f4c8d96fc9a8f
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-19 06:50:01 -0700

    fixing ‘int’ and ‘size_t’ signedness error

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - a function that executes a function given 
+ * array_iterator - a function that executes a function given
  * as a parameter on each element of an array
  *
  * @array: pointer to the array
@@ -14,7 +14,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
 	if (array == NULL || action == NULL)
 	{
