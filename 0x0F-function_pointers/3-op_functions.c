34f92fd39a32a4e3bd1eae89a537f14b883b9ec5
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-19 15:50:47 -0700

    headere file and simple operations func

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - a function that adds integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of integers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - a function that substructs two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - a function that multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - a function that divides two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: The quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - a function that remainder after division
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
