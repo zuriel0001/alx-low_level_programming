77bb27de3a14a56f7ba4dfee27f38e665f933c9f
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-27 19:32:03 +0000

    a function that before the main function is executed.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_b4_main - a function to prjnt string before main
+ * Return: nothing
+ */
+
+void print_b4_main (void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+
+	printf("I bore my house upon my back!\n");
+}
