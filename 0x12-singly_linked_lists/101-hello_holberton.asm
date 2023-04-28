d60f2706bd75f2aa2a4ce72f837f5d2d294f6400
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-28 01:49:38 +0000

    fixed error

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,12 @@
-/* assembly code in C **/
-
 global    main
           extern    printf
+
 main:
 	  mov   edi, format
 	  xor   eax, eax
 	  call  printf
 	  mov 	eax, 0
 	  ret
+
 format: db `Hello, Holberton\n`,0
 
