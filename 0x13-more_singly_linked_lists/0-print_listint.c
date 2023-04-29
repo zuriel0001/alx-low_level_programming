6e9399e4de8a6479dca31be0f18dd731d608c05f
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-29 16:04:26 +0000

    fixed betty error

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 #include <main.h>
 
 /**
- * print_listint -  a function that prints all the elements of a "listint_t" list
+ * print_listint - a function that prints all the elements of "listint_t" list
  *
  * @h: pointer to struct "listint_s"
  *
@@ -12,7 +12,7 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t i /* number of nodes */
-	
+
 	for (; h; h = h->next)
 	{
 		printf("%d\n", h->n);
