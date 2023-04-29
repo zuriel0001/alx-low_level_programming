02c59f57058a83b20f9b15031feea26a65527143
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-29 17:01:39 +0000

    change for to while loop

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,12 +12,10 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i = 0; /* number of nodes */
 
-	if (!h)
-		return (NULL);
-
-	for (; h; h = h->next)
+	while (h)
 	{
 		printf("%d\n", h->n);
+		h = h->next;
 		i++;
 	}
 	return (i);
