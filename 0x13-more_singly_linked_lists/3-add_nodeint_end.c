e17f95dc22a221cbd98666d26cf14f21be7e19d8
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-29 23:34:54 +0000

    add node to the tail

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ *add_nodeint_end - function that adds a new node at the end of a list
+ *
+ *@head: pointer to first node
+ *
+ *@n: integer to he added
+ *
+ *Return: pointer to new element, or NULL if it failed
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node;
+	listint_t *node_holder;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	(new_node).n = n;
+	(new_node).next == NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	node_holder = *head;
+	while ((*node_holder).next)
+	{
+		node_holder = (*node_holder).next;
+		(*node_holder).next = new_node;
+	}
+	return (new_node);
+}
