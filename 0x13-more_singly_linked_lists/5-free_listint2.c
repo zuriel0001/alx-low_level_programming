ce11b80282b6ac8ca738f112d7405978348a50f4
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-30 19:05:35 +0000

    free singly linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 -  a function that frees a listint_t list
+ *
+ * @head: pointer to first node pointer
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node_pointer;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		*head = node_pointer;
+		node_pointer = (*head).next;
+		free(node_pointer);
+	}
+	*head = NULL;
+}
+
