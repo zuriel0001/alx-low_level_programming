0d563d8cec5154d309db5fabaedce502af16860d
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-30 19:45:18 +0000

    fixed output error

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,7 +13,7 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 		return;
 
-	for (; *head != NULL; *head = (**head).next)
+	for (; *head; *head = (**head).next)
 	{
 		node_pointer = *head;
 		/* *head = (**head).next; */
