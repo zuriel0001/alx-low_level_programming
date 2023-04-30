8e4e5afec354010c36df05e30a8a8a579a726daa
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-04-30 16:39:13 +0000

    while loop block edited

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,7 +21,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 
 	(*new_node).n = n;
-	(*new_node).next == NULL;
+	(*new_node).next = NULL;
 
 	if (*head == NULL)
 	{
@@ -33,7 +33,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	while ((*node_holder).next)
 	{
 		node_holder = (*node_holder).next;
-		(*node_holder).next = new_node;
 	}
+	(*node_holder).next = new_node;
 	return (new_node);
 }
