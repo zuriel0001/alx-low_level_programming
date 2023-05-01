8a09fdb51d58d1012379a391c09e3c47cce1b93d
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-05-01 21:15:43 +0000

    fixed output

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 			return (-1);
 		}
-		any_node->next = any_node->next;
+		any_node = any_node->next;
 	}
 
 	new_node = any_node->next;
