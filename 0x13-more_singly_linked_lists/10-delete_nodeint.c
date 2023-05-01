c7c8e25da7fb4d465010b156d1c74c10338ba580
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-05-01 20:52:00 +0000

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *new_node = NULL;
+	listint_t *new_node;
 	listint_t *any_node;
 
 	if (*head == NULL)
