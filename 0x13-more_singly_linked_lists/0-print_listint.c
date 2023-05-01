c0ac54599b87bb0ebab69f391fc2ee30fb7c3914
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-05-01 22:38:47 +0000

    node count

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -15,11 +15,7 @@ size_t print_listint(const listint_t *h)
 	for (; h != NULL; h = h->next)
 	{
 		n_count++;
-
-		if (h->n)
-			printf("%d\n", h->n);
-		else
-			break;
+		printf("%d\n", h->n);
 	}
 	return (n_count);
 }
