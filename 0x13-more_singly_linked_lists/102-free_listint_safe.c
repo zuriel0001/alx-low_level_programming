4444ab6eb9f4fad1b676fad12a563987a59eb6cd
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-05-02 15:56:37 +0000

    Free (safe version)

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe -  a function that frees a listint_t list
+ * @h: pointer to firdt node
+ *
+ * Return: the size of the list that was free’d
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *temp_p;/* create a temporary pointer */
+	size_t list_len; /* store the length of the list */
+	int diff;
+	/**
+	 * diff store the difference in memory address
+	 * between current node and next node
+	 */
+
+	if (!*h)
+		return (0);
+
+	list_len = 0;
+
+	for (; *h; list_len++)
+	{	/* calculate the difference in memory address */
+
+		diff = *h - (*h)->next;
+
+		if (diff > 0)/* if the difference is greater than 0, there is a loop */
+		{
+			temp_p = (*h)->next;
+			free(*h);
+			*h = temp_p;
+		}
+		else
+			free(*h);
+			*h = NULL;
+			break;
+	}
+	*h = NULL;
+
+	return (list_len);
+}
