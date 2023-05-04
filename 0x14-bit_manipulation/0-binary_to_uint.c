ac62010d520e3747ca55a16d3bbf718b9d9d07e5
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-05-04 21:22:44 +0000

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -21,12 +21,12 @@ unsigned int binary_to_uint(const char *b)
 	num = 0;
 	while (b[i])
 	{
-		i++;
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
 
 		num = 2 * num + (b[i] - '0');
 	}
+        i++;
 
 	return (num);
 }
