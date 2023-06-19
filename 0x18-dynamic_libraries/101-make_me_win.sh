ee65fd5402582b38b0c718cb0eeadf3760c70fcb
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-06-19 20:10:14 +0000

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/zuriel0001/holbertonschool-low_level_programming/main/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -q --output-document=$HOME/win.so https://github.com/zuriel0001/alx-low_level_programming/main/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=$HOME/win.so
