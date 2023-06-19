ede151160d31cd4e3ccbcad5cc2172e8658c6e3c
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-06-19 08:07:27 +0000

    header, functions and readme

---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn -  function that gets the length of a prefix substring
+ * @s: string
+ * @accept: a section of s
+ * Return: number of bytes in the initial segment of `s` which consist
+ *         entirely of bytes from `accept`.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	char *p;
+
+	while (*s)
+	{
+		for (p = accept; *p; p++)
+		{
+			if (*s == *p)
+			{
+				n++;
+				break;
+			}
+		}
+		if (!*p)
+			break;
+		s++;
+	}
+	return (n);
+}
