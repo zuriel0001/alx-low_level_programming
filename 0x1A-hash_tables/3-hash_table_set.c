3fe50e3209255a6aa58f8538b0612219c22a1e07
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-07-07 10:24:41 -0700

    a function that adds an element to the hash table.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,53 +1,55 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Add or update an element in a hash table.
- * @ht: A pointer to the hash table.
- * @key: The key to add - cannot be an empty string.
- * @value: The value associated with key.
+ * hash_table_set - a function that adds an element to the hash table
  *
- * Return: Upon failure - 0.
- *         Otherwise - 1.
+ * @ht: A pointer to the hash table.
+ * @key: The key to be  added - cannot be an empty string.
+ * @value: The value key hold
+ *
+ * Return: 0 upon failure, Otherwise 1.
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
-	char *value_copy;
+	hash_node_t *new_node; /* Pointer to a new hash node */
+	char *tmp_value; /* Temporary value for storing a copy of 'value' */
 	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	value_copy = strdup(value);
-	if (value_copy == NULL)
+	tmp_value = strdup(value);
+	if (tmp_value == NULL)
 		return (0);
 
+	/* Get the index using the key */
 	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
-	{
+
+	/* Iterate through the array at the index */
+	i = index;
+	while (ht->array[i])
+	{       /* Check if the key already exists */
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
+			ht->array[i]->value = tmp_value;
 			return (1);
 		}
-	}
-
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+		i++; }
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
 	{
-		free(value_copy);
-		return (0);
-	}
-	new->key = strdup(key);
-	if (new->key == NULL)
+		free(tmp_value);
+		return (0); }
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
 	{
-		free(new);
-		return (0);
-	}
-	new->value = value_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
+		free(new_node);
+		return (0); }
+	new_node->value = tmp_value;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
 	return (1);
 }
