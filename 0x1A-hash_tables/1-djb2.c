5f0b67b26f92985f5900d376c148d37930b42f51
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-07-07 15:26:17 +0000

    Update 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -10,12 +10,12 @@
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int cal_hash; /*  stores the calculated hash */
-	int c_ters;  /* Variable to store each character of the string */
+	int c;  /* Variable to store each character of the string */
 
 	cal_hash = 5381;
 
-	while ((c_ters = *str++))
-		cal_hash = ((cal_hash << 5) + cal_hash) + c_ters; /* hash * 33 + c */
+	while ((c = *str++))
+		cal_hash = ((cal_hash << 5) + cal_hash) + c; /* hash * 33 + c */
 
 	return (cal_hash);
 }
