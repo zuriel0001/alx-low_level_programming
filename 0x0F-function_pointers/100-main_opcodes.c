bdb2d6a3aaeb379fbca160dde566cb6d31f6e398
zuriel0001 <dogbeykwame.kd@gmail.com>
2023-07-09 14:49:17 -0700

    a program that prints the opcodes of its own main function

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -7,7 +7,7 @@
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
