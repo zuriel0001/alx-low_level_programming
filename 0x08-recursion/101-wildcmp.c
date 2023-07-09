e165e7afa5eda267bfb968696bff780e9d7cbad8
zuriel0001 <125594301+zuriel0001@users.noreply.github.com>
2023-07-09 17:39:34 +0000

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,11 +11,11 @@
 int wildcmp(char *s1, char *s2)
 {
     /* Base case: If both strings are empty, they are identical */
-    if (*s1 == '\0' && *s2 == '\0')
+    if (*s1 == '\0' && (*s2 == '\0' || (*s2 == '*' && *(s2 + 1) == '\0')))
         return (1);
 
     /* If the current characters match or if the wildcard character is present */
-    if (*s1 == *s2 || (*s2 == '*' && (*(s2 + 1) == '\0' || *(s2 + 1) == '*') && *s1 != '\0'))
+    if (*s1 == *s2 || (*s2 == '*' && *s1 != '\0'))
     {
         /* Move to the next characters in both strings */
         return (wildcmp(s1 + 1, s2 + 1));
